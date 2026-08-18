3bd45f90f05144248d28899dd5f77b9e0bb88e7c
laeva95 <dnskrdl123@gmail.com>
2026-08-18 15:59:03 +0900

    2026-08-18 jsp 수업 시작, 첫 서블릿 생성

---
+++ pro05/src/main/java/sec01/ex01/FirstServlet.java
@@ -0,0 +1,22 @@
+package sec01.ex01;
+
+import javax.servlet.http.HttpServlet;
+
+/*
+ 
+ 	1. 실제 웹 프로그래밍에서 사용되는 사용자 정의 서블릿 FirstServlet 생성
+ 
+ 		ServletApi.jar 라이브러리에서 제공되는 HttpServlet 클래스를 상속 받아서 생성
+ 		
+ 	2. HttpServlet 클래스에서 제공하는 서블릿 생명주기 메소드
+ 		
+ 		init(), service(), doGet(), doPost(), destroy()
+ 
+ */
+public class FirstServlet extends HttpServlet{
+	
+	
+	
+	
+	
+}
